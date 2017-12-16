1f50e5151b729fa92feeede45137067782f22569
Administrator <cody_chen@163.com>
2017-12-16 10:27:44 +0800

    selenium UI Auto_Test

---
+++ webdriver.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+browser = webdriver.Chrome()
+browser.get('http://www.zhihu.com/')
+assert "知乎" in browser.title
+
+browser.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/a[2]").click()
+browser.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[1]/div[1]/div[2]/span").click()
+browser.implicitly_wait(10)
+browser.find_element_by_name("account").send_keys("cody_chen@163.com")
+browser.find_element_by_name("password").send_keys("cody.33231")
+browser.find_element_by_name("password").send_keys(Keys.RETURN)
+
+
+#browser.quit()
+
